4a1b3e529f63f987ca104737d683cf560d028a45
Sam Phillips <samdphillips@gmail.com>
2022-05-12 20:24:39 -0700

    Make series a concrete type.

--- uke/series.rkt
+++ uke/series.rkt
@@ -1,7 +1,6 @@
 #lang racket/base
 
-(require racket/generic
-         racket/unsafe/ops
+(require racket/unsafe/ops
          racket/vector
          "index.rkt"
          "store.rkt"
@@ -19,37 +18,16 @@
          vector->series
          sequence->series)
 
-(define-generics series
-  ;; series-name :: Series -> String / #f
-  (series-name series)
-
-  ;; series-index :: Series -> Index
-  (series-index series)
-
-  ;; Series -> Store
-  (series-store series)
-
-  ;; Series K -> V
-  (series-ref series k)
-
-  #:fallbacks
-  [(define/generic ^series-index series-index)
-   (define/generic ^series-store series-store)
-   (define (series-ref s i)
-     (define j (index-lookup (^series-index s) i))
-     (store-ref (^series-store s) j))])
-
 (define (series-inspect s)
   (list 'series
         (cons 'name (series-name s))
         (index-inspect (series-index s))
         (cons 'store (series-store s))))
 
-(struct basic-series (name index store)
-  #:methods gen:series
-  [(define (series-name s) (basic-series-name s))
-   (define (series-index s) (basic-series-index s))
-   (define (series-store s) (basic-series-store s))])
+(struct series (name index store) #:transparent)
+
+(define (series-ref a-series i)
+  (store-ref (series-store a-series) (index-lookup (series-index a-series) i)))
 
 (define (->series name seq)
   (cond
@@ -65,7 +43,7 @@
                [i (in-naturals)])
       (vector-set! store i v)
       (hash-set index k i)))
-  (basic-series name index (vector->immutable-vector store)))
+  (series name index (vector->immutable-vector store)))
 
 ;; XXX An index could be used to just use subset of the elements?  Also it
 ;; could handle a slice?  Probably better than current interface.
@@ -78,19 +56,13 @@
           (if size
               (vector-copy a-vector 0 size)
               (vector-copy a-vector)))))
-  (basic-series name
-                (seq-identity-index series-size)
-                series-vector))
+  (series name (seq-identity-index series-size) series-vector))
 
 (define (sequence->series name seq)
   (define-values (store len)
     (sequence->list/length seq))
-  (basic-series name
-                (seq-identity-index len)
-                (list->immutable-vector store)))
+  (series name (seq-identity-index len) (list->immutable-vector store)))
 
-;; XXX only implemented for basic-series, maybe series should be a struct type
-;; not a generic.
 (define (series-push-index a-series an-index)
   (define new-index (index-compose an-index (series-index a-series)))
-  (struct-copy basic-series a-series [index new-index]))
+  (struct-copy series a-series [index new-index]))
