ea43d0d30ff95f820220aa617dfbd6375bb201a5
Sam Phillips <samdphillips@gmail.com>
2023-10-08 12:46:01 -0700

    Convert index api

---
+++ uke-test/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define name "uke-test")
+(define collection "uke")
+(define version "0.1")
+(define deps '("base" "uke"))
+(define build-deps '("base" "rackunit-lib"))
+(define pkg-authors '(samdphillips@gmail.com))
--- uke/error.rkt
+++ uke/error.rkt
@@ -1,9 +1,11 @@
 #lang racket/base
 
 (provide (struct-out exn:uke)
+         (struct-out exn:uke:index)
          raise-uke-error)
 
 (struct exn:uke exn:fail ())
+(struct exn:uke:index exn:uke ())
 
 (define (raise-uke-error make-exn name message . args)
   (define final-message
