b57fc617f75ca9bbbd92727d74b5abce41c4a452
Sam Phillips <samdphillips@gmail.com>
2023-10-08 12:46:01 -0700

    Convert index api

---
+++ uke-test/test/index.rkt
@@ -0,0 +1,89 @@
+#lang racket/base
+
+(require racket/sequence
+         rackunit
+         uke/index)
+
+(test-case "linear identity index"
+  (define idx (make-linear-index 10))
+  (check-equal? (index-size idx) 10)
+  (check-equal? (sequence->list (in-indices idx))
+                '(0 1 2 3 4 5 6 7 8 9))
+  (check-equal? (index-ref idx 0) 0)
+  (check-equal? (index-ref idx 9) 9))
+
+(test-case "linear-index oob lookup"
+  (define idx (make-linear-index 10))
+  (check-exn exn:uke:index? (lambda () (index-ref idx -1)))
+  (check-exn exn:uke:index? (lambda () (index-ref idx 10)))
+  (check-exn exn:uke:index? (lambda () (index-ref idx 100))))
+
+(test-case "identity/identity same index compose"
+  (define i (make-linear-index 10))
+  (define j (index-compose i i))
+  (check-equal? (index-size j) 10))
+
+(test-case "identity/identity smaller index compose"
+  (define i (make-linear-index 10))
+  (define j (make-linear-index 20))
+  (define k (index-compose j i))
+  (check-equal? (index-size k) 10)
+  (check-exn exn:uke:index? (lambda () (index-compose i j))))
+
+(check-equal? (index-size (make-vector-index #())) 0)
+
+(test-case "vector-index"
+  (define idx (make-vector-index (vector-immutable 3 2 1 0)))
+  (check-equal? (sequence->list (in-indices idx)) '(0 1 2 3))
+  (check-equal? (index-size idx) 4)
+  (check-equal? (index-ref idx 0) 3)
+  (check-equal? (index-ref idx 1) 2)
+  (check-equal? (index-ref idx 2) 1)
+  (check-equal? (index-ref idx 3) 0)
+  (check-exn exn:uke:index? (lambda () (index-ref idx -1)))
+  (check-exn exn:uke:index? (lambda () (index-ref idx 4))))
+
+(test-case "vector/vector index compose"
+  (define i (make-vector-index (vector-immutable 3 2 1 0)))
+  (define j (make-vector-index (vector-immutable 0 1 2 3)))
+
+  (define ii (index-compose i i))
+  (check-true (vector-index? ii))
+  (check-true (immutable? (vector-index-mapping ii)))
+
+  (define jj (index-compose j j))
+  (check-true (vector-index? jj))
+  (check-true (immutable? (vector-index-mapping jj)))
+
+  (check-equal? ii j)
+  (check-equal? jj j))
+
+(test-case "identity/vector index compose"
+  (define i (make-linear-index 3))
+  (define j (make-vector-index (vector-immutable 2 1 0)))
+
+  (define ij (index-compose i j))
+  (check-true (vector-index? ij))
+  (check-true (immutable? (vector-index-mapping ij)))
+  (check-equal? (index-size ij) 3)
+  (check-equal? (for/list ([t (in-indices ij)])
+                  (index-ref ij t))
+                '(2 1 0))
+
+  (define ji (index-compose j i))
+  (check-equal? (index-size ji) 3)
+  (check-equal? (for/list ([t (in-indices ji)])
+                  (index-ref ji t))
+                '(2 1 0)))
+
+(test-case "identity/vector smaller index compose"
+  (define i (make-linear-index 3))
+  (define j (make-vector-index (vector-immutable 3 2 1 0)))
+  (define ji (index-compose j i))
+  (check-true (vector-index? ji))
+  (check-true (immutable? (vector-index-mapping ji)))
+  (check-equal? (index-size ji) 3)
+  (check-equal? (for/list ([t (in-indices ji)])
+                  (index-ref ji t))
+                '(3 2 1))
+  (check-exn exn:uke:index? (lambda () (index-compose i j))))
--- uke/index.rkt
+++ uke/index.rkt
@@ -1,247 +1,127 @@
 #lang racket/base
 
-(require racket/generic
+(require (for-syntax racket/base
+                     syntax/parse)
          racket/unsafe/ops
-         "error.rkt"
-         "util.rkt")
+         "error.rkt")
 
-(provide gen:index
+(provide prop:index
          index?
-         index-lookup
-         index-inspect
-         index-indices
-         index-sequential?
+         index-ref
          index-size
          index-compose
+         in-indices
 
          exn:uke:index?
 
-         (struct-out seq-identity-index))
+         vector-index?
+         linear-index?
 
-(module+ test
-  (require racket/sequence
-           racket/set
-           rackunit))
+         make-vector-index
+         vector-index-mapping
 
-(struct exn:uke:index exn:uke ())
+         make-linear-index
+         linear-index-offset
+         linear-index-stride)
 
-(define-generics index
-  ;; index-lookup :: Index i -> j
-  (index-lookup index i)
+;; index-ref
+;; index-size
+;; index-compose
+;; XXX maybe needs property guard
+(define-values (prop:index index? index-ops)
+  (make-struct-type-property 'index))
 
-  ;; index-indices :: Index -> Sequenceof i
-  (index-indices index)
+(define (get-index-op idx slot)
+  (vector-ref (index-ops idx) slot))
 
-  ;; index-size :: Index -> Integer
-  (index-size index)
+(define (apply-index-op idx slot . args)
+  (apply (get-index-op idx slot) idx args))
 
-  ;; index-compose :: Index Index -> Index
-  (index-compose index other-index)
+(define (check-index-bounds who idx i)
+  (unless (and (<= 0 i) (< i (index-size idx)))
+    (raise-uke-error exn:uke:index who "index out of range for index: ~a" i)))
 
-  ;; index-sequential? :: Index -> Boolean
-  (index-sequential? index)
+(define (check-index-sizes-compatible who i0 i1)
+  (unless (<= (index-size i0) (index-size i1))
+    (raise-uke-error exn:uke:index
+                     who
+                     "index sizes incompatible: ~a > ~a"
+                     (index-size i0)
+                     (index-size i1))))
 
-  #:defaults
-  ([immutable-vector?
-    (define/generic ^index-lookup index-lookup)
-    (define/generic ^index-size index-size)
+(define (index-ref idx i)
+  (check-index-bounds 'index-ref idx i)
+  (apply-index-op idx 0 i))
 
-    (define (index-lookup a-vector pos)
-      (unless (vector-valid-position? a-vector pos)
-        (raise-uke-error exn:uke:index
-                        'index-lookup
-                        "lookup index is out of bounds: ~a"
-                        pos))
-      (vector-ref a-vector pos))
+(define (index-size idx)
+  (apply-index-op idx 1))
 
-    (define (index-indices vec)
-      (in-range 0 (vector-length vec)))
+(define (index-compose i0 i1)
+  (check-index-sizes-compatible 'index-compose i1 i0)
+  (cond
+    [(get-index-op i0 2) => (λ (f) (f i0 i1))]
+    [else (generic-index-compose i0 i1)]))
 
-    (define (index-size vec)
-      (vector-length vec))
+;; XXX in-indices should probably work on indexes, series, and dataframes.
+(define in-indices-sequence
+  (procedure-rename
+   (λ (idx)
+     (in-range (index-size idx)))
+   'in-indices))
 
-    (define (index-compose a-vector an-index)
-      (unsafe-vector*->immutable-vector!
-        (for/vector #:length (vector-length a-vector) ([i (in-vector a-vector)])
-          (^index-lookup an-index i))))
+(define-sequence-syntax in-indices
+  (λ () #'in-indices-sequence)
+  (syntax-parser
+    [(v:id (in-indices an-index)) #'(v (in-range (index-size an-index)))]
+    [_ #f]))
 
-    (define (index-sequential? vec) #t)]
+(define (generic-index-compose i0 i1)
+  (define n (index-size i1))
+  (define vec
+    (unsafe-vector*->immutable-vector!
+     (for/vector #:length n ([i (in-range n)])
+       (index-ref i0 (index-ref i1 i)))))
+  (vector-index vec))
 
-   [immutable-hash?
-    (define/generic ^index-lookup index-lookup)
+(define (linear-index-ref idx i)
+  (check-index-bounds 'index-ref idx i)
+  (+ (linear-index-offset idx)
+     (* (linear-index-stride idx) i)))
 
-    (define (index-lookup a-hash key)
-      (or (hash-ref a-hash key #f)
-          (raise-uke-error exn:uke:index
-                           'index-lookup
-                           "lookup key is not in index: ~a"
-                           key)))
+(define (linear-index-compose i0 i1)
+  (cond
+    [(not (linear-index? i1)) (generic-index-compose i0 i1)]
+    [else (define o0 (linear-index-offset i0))
+          (define s0 (linear-index-stride i0))
+          (define o1 (linear-index-offset i1))
+          (define s1 (linear-index-stride i1))
+          (linear-index (index-size i1)
+                        (+ o0 (* s0 o1))
+                        (* s0 s1))]))
 
-    (define (index-indices hsh)
-      (in-hash-keys hsh))
-
-    (define (index-size hsh)
-      (hash-count hsh))
-
-    (define (index-compose a-hash idx)
-      (for/hash ([(k v) (in-hash a-hash)])
-        (values k (^index-lookup idx v))))
-
-    (define (index-sequential? hsh) #f)]))
-
-(define (index-inspect i)
-  (list 'index (cons 'size (index-size i)) i))
-
-(module+ test
-  (check-equal? (sequence->list (index-indices (vector-immutable))) null)
-
-  (test-case "vector-immutable index"
-    (define idx (vector-immutable 3 2 1 0))
-    (check-equal? (sequence->list (index-indices idx)) '(0 1 2 3))
-    (check-true (index-sequential? idx))
-    (check-equal? (index-size idx) 4)
-    (check-equal? (index-lookup idx 0) 3)
-    (check-equal? (index-lookup idx 1) 2)
-    (check-equal? (index-lookup idx 2) 1)
-    (check-equal? (index-lookup idx 3) 0)
-    (check-exn exn:uke:index? (lambda () (index-lookup idx -1)))
-    (check-exn exn:uke:index? (lambda () (index-lookup idx 4))))
-
-  (test-case "vector/vector index compose"
-    (define i (vector-immutable 3 2 1 0))
-    (define j (vector-immutable 0 1 2 3))
-    (check-equal? (index-compose i i) j)
-    (check-equal? (index-compose j j) j))
-
-  (test-case "hash index"
-    (define idx (hash 'a 0 'b 1 'c 2 'd 3))
-    (check-equal? (index-size idx) 4)
-    (check-equal? (for/set ([v (index-indices idx)]) v) (set 'a 'b 'c 'd))
-    (check-false (index-sequential? idx))
-    (check-equal? (index-lookup idx 'd) 3)
-    (check-equal? (index-lookup idx 'c) 2)
-    (check-equal? (index-lookup idx 'b) 1)
-    (check-equal? (index-lookup idx 'a) 0)
-    (check-exn exn:uke:index? (lambda () (index-lookup idx 'e))))
-
-  (test-case "hash/hash index compose"
-    (define i (hash 'a 'd 'b 'c 'c 'b 'd 'a))
-    (define j (hash 'a 'a 'b 'b 'c 'c 'd 'd))
-    (check-equal? (index-compose i i) j)
-    (check-equal? (index-compose j j) j))
-
-  (test-case "vector/hash index compose"
-    (define i (vector-immutable 'a 'b 'c 'd))
-    (define j (hash 'a 3 'b 2 'c 1 'd 0))
-    (check-equal? (index-compose i j)
-                  (vector-immutable 3 2 1 0))
-    (check-equal? (index-compose j i)
-                  (hash 'a 'd 'b 'c 'c 'b 'd 'a))))
-
-(struct seq-identity-index (size)
+(struct linear-index (size offset stride)
   #:transparent
-  #:methods gen:index
-  [(define/generic ^index-lookup index-lookup)
-   (define/generic ^index-sequential? index-sequential?)
-   (define/generic ^index-size index-size)
+  #:property prop:index
+  (vector linear-index-ref
+          (λ (idx) (linear-index-size idx))
+          linear-index-compose))
 
-   (define (index-lookup id-idx pos)
-     (unless (and (<= 0 pos) (< pos (seq-identity-index-size id-idx)))
-       (raise-uke-error exn:uke:index
-                        'index-lookup
-                        "lookup index is out of bounds:\n  index-size: ~a\n  position: ~a"
-                        (seq-identity-index-size id-idx)
-                        pos))
-     pos)
+(define (make-linear-index size [offset 0] [stride 1])
+  (linear-index size offset stride))
 
-   (define (index-indices id-idx)
-     (in-range (seq-identity-index-size id-idx)))
+(define (vector-index-ref idx i)
+  (check-index-bounds 'index-ref idx i)
+  (vector-ref (vector-index-mapping idx) i))
 
-   (define (index-size id-idx)
-     (seq-identity-index-size id-idx))
+(define (vector-index-size idx)
+  (vector-length (vector-index-mapping idx)))
 
-   (define (index-compose id-idx j)
-     (unless (^index-sequential? j)
-       (raise-uke-error exn:uke:index
-                        'index-compose
-                        "cannot compose non-sequential index with identity index"))
-     (define id-idx-size (seq-identity-index-size id-idx))
-     (when (< (^index-size j) id-idx-size)
-       (raise-uke-error exn:uke:index
-                        'index-compose
-                        "indexes are incompatible sizes ~a > ~a"
-                        id-idx-size
-                        (^index-size j)))
-     (cond
-       [(seq-identity-index? j) id-idx]
-       [else
-        (unsafe-vector*->immutable-vector!
-         (for/vector #:length id-idx-size ([i (in-range id-idx-size)])
-           (^index-lookup j i)))]))
+(struct vector-index (mapping)
+  #:transparent
+  #:property prop:index
+  (vector vector-index-ref
+          vector-index-size
+          #f))
 
-   (define (index-sequential? ii) #t)])
-
-(module+ test
-  (test-case "seq-identity-index"
-    (define idx (seq-identity-index 10))
-    (check-true (index-sequential? idx))
-    (check-equal? (index-size idx) 10)
-    (check-equal? (sequence->list (index-indices idx))
-                  '(0 1 2 3 4 5 6 7 8 9))
-    (check-equal? (index-lookup idx 0) 0)
-    (check-equal? (index-lookup idx 9) 9))
-
-  (test-case "seq-identity-index oob lookup"
-    (define idx (seq-identity-index 10))
-    (check-exn exn:uke:index? (lambda () (index-lookup idx -1)))
-    (check-exn exn:uke:index? (lambda () (index-lookup idx 10)))
-    (check-exn exn:uke:index? (lambda () (index-lookup idx 100))))
-
-  (test-case "identity/identity same index compose"
-    (define i (seq-identity-index 10))
-    (define j (index-compose i i))
-    (check-equal? (index-size j) 10))
-
-  (test-case "identity/identity smaller index compose"
-    (define i (seq-identity-index 10))
-    (define j (seq-identity-index 20))
-    (define k (index-compose i j))
-    (check-equal? (index-size k) 10)
-    (check-exn exn:uke:index? (lambda () (index-compose j i))))
-
-  (test-case "identity/vector index compose"
-    (define i (seq-identity-index 3))
-    (define j (vector-immutable 2 1 0))
-
-    (define ij (index-compose i j))
-    (check-true (index-sequential? ij))
-    (check-equal? (index-size ij) 3)
-    (check-equal? (for/list ([t (index-indices ij)])
-                    (index-lookup ij t))
-                  '(2 1 0))
-
-    (define ji (index-compose j i))
-    (check-true (index-sequential? ji))
-    (check-equal? (index-size ji) 3)
-    (check-equal? (for/list ([t (index-indices ji)])
-                    (index-lookup ji t))
-                  '(2 1 0)))
-
-  (test-case "identity/vector smaller index compose"
-    (define i (seq-identity-index 3))
-    (define j (vector-immutable 3 2 1 0))
-
-    (define ij (index-compose i j))
-    (check-equal? (index-size ij) 3)
-    (check-equal? (for/list ([t (index-indices ij)])
-                    (index-lookup ij t))
-                  '(3 2 1))
-
-    (check-exn exn:uke:index? (lambda () (index-compose j i))))
-
-  (test-case "identity/hash index compose errors"
-    (define i (seq-identity-index 3))
-    (define j (hash 'a 0 'b 1 'c 2))
-    (check-exn exn:uke:index? (lambda () (index-compose i j))))
-
-    )
+(define (make-vector-index vec)
+  (vector-index (vector->immutable-vector vec)))
