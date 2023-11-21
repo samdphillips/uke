6de78afb89961d2d9c27aca0bb8e0a23c89aa3d7
Sam Phillips <samdphillips@gmail.com>
2023-11-21 14:04:30 -0800

    Remove sawzall-alike packages

--- uke-sawzall/info.rkt
+++
@@ -1,7 +0,0 @@
-#lang info
-
-(define name "uke-sawzall")
-(define collection "uke")
-(define version "0.1")
-(define deps '("base" "uke" "uni-table"))
-(define pkg-authors '(samdphillips@gmail.com))
--- uke-test/info.rkt
+++ uke-test/info.rkt
@@ -3,6 +3,6 @@
 (define name "uke-test")
 (define collection "uke")
 (define version "0.1")
-(define deps '("base" "uke" "uke-sawzall"))
+(define deps '("base" "uke" "uke-machete"))
 (define build-deps '("base" "rackunit-lib"))
 (define pkg-authors '(samdphillips@gmail.com))
