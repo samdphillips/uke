27443144ee758f470d4808fb4b34ec885fb85503
Sam Phillips <samdphillips@gmail.com>
2025-03-09 20:08:48 -0700

    Change dep in `uke-extra-lib` to `uke-lib`

--- uke-extra-lib/info.rkt
+++ uke-extra-lib/info.rkt
@@ -3,6 +3,6 @@
 (define name "uke-extra-lib")
 (define collection "uke")
 (define version "0.1")
-(define deps '("base" "uke"))
+(define deps '("base" "uke-lib"))
 (define build-deps '("base" "csv-reading"))
 (define pkg-authors '(samdphillips@gmail.com))
