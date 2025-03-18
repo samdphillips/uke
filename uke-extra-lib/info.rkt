2a41bcb4dc82167a8739634a47abf28cc9411475
Sam Phillips <samdphillips@gmail.com>
2025-03-17 18:10:28 -0700

    Remove unused build-deps

--- uke-extra-lib/info.rkt
+++ uke-extra-lib/info.rkt
@@ -4,5 +4,4 @@
 (define collection "uke")
 (define version "2025.3.14")
 (define deps '("base" "csv-reading" "uke-lib"))
-(define build-deps '("base"))
 (define pkg-authors '(samdphillips@gmail.com))
--- uke-lib/info.rkt
+++ uke-lib/info.rkt
@@ -4,5 +4,4 @@
 (define collection "uke")
 (define version "2025.3.14")
 (define deps '("base"))
-(define build-deps '("base"))
 (define pkg-authors '(samdphillips@gmail.com))
