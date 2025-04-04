227abacd8911eaa5b70c2d5cefe7b38258c3cccc
Sam Phillips <samdphillips@gmail.com>
2025-04-04 11:09:13 -1000

    Add `racket-doc` dep to uke package

--- uke/info.rkt
+++ uke/info.rkt
@@ -4,6 +4,6 @@
 (define collection "uke")
 (define version "2025.3.14")
 (define deps '("base" "uke-lib" "uke-extra-lib" "uke-machete" "rackunit-lib"))
-(define build-deps '("scribble-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
 (define pkg-authors '(samdphillips@gmail.com))
 (define scribblings '(("scribblings/uke.scrbl" ())))
