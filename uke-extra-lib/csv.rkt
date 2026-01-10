80544fe6fb473376853b4931ffe6fc20be226020
Sam Phillips <samdphillips@gmail.com>
2026-01-10 12:38:36 -0800

    [csv] add option to handle mismatched column counts

--- uke-extra-lib/csv.rkt
+++ uke-extra-lib/csv.rkt
@@ -11,6 +11,7 @@
 
 (define (csv->dataframe inp
                         [reader-spec null]
+                        #:column-mismatch [col-mismatch 'skip]
                         #:column-names [col-names 'first])
   (define next-row (make-csv-reader inp reader-spec))
   (define (list->vector* vs)
@@ -34,10 +35,26 @@
                                 (make-producer)))]
       [(list? col-names)
        (values col-names (make-producer))]))
+  (define check-column-mismatch
+    (let ([ncols (length names)])
+      (cond
+        [(eq? col-mismatch 'skip)
+         (λ (v) (= (vector-length v) ncols))]
+        [(eq? col-mismatch 'error)
+         (λ (v)
+           (cond
+             [(= (vector-length v) ncols) #t]
+             [else
+              (error 'csv->dataframe
+                     (string-append
+                      "incorrect number of column values for row\n"
+                      " expected-count: ~a\n"
+                      " row: ~s")
+                     ncols v)]))])))
   (define ((col-ref i) v) (vector-ref v i))
   (define store
     (unsafe-vector*->immutable-vector!
-     (for/vector ([r rows]) r)))
+     (for/vector ([r rows] #:when (check-column-mismatch r)) r)))
   (define idx (make-linear-index (vector-length store)))
   (define col*
     (for/list ([name (in-list names)]
